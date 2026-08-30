85c50de8c0ed6432a4ca7fd9b0b91aab2a4ba04f
vemulasomeswararao24-oss <vemulasomeswararao24@gmail.com>
2026-08-30 14:03:50 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,29 @@
-tg
+print("Simple Calculator")
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = input("Enter your choice: ")
+
+if choice == "1":
+    print("Answer:", a + b)
+
+elif choice == "2":
+    print("Answer:", a - b)
+
+elif choice == "3":
+    print("Answer:", a * b)
+
+elif choice == "4":
+    if b != 0:
+        print("Answer:", a / b)
+    else:
+        print("Cannot divide by zero")
+
+else:
+    print("Invalid choice")
